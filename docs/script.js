26cdcefd471fd0c343d1c1f254871c7c0b63f147
Rikson <noskir123@yahoo.co.jp>
2017-10-31 23:11:06 +0800

    Add demo into doc

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,10 @@
 require('bootstrap');
+require('sticky-kit/dist/sticky-kit.js');
 require('../jquery.inifinite-scroll-up-n-down.js');
 
 $(function(){
+	$("#sidenav").stick_in_parent()
+
 	$('#page').infiniteScrollUND({
 		upNav: '.previous a',
 		downNav: '.next a',
